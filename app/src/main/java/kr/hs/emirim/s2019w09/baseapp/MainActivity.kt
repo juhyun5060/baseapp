8ef4287e57a1e5249881e47b23610e11fce5c4ce
Juhyun <s2019w09@e-mirim.hs.kr>
2020-09-22 21:04:11 +0900

    버튼과 체크박스 누르면 토스트로 알림 뜨는 기능 구현

--- app/src/main/java/kr/hs/emirim/s2019w09/baseapp/MainActivity.kt
+++ app/src/main/java/kr/hs/emirim/s2019w09/baseapp/MainActivity.kt
@@ -1,11 +1,39 @@
 package kr.hs.emirim.s2019w09.baseapp
 
-import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.Button
+import android.widget.CheckBox
+import android.widget.Toast
+import androidx.appcompat.app.AppCompatActivity
 
 class MainActivity : AppCompatActivity() {
+
+    lateinit var button1 : Button
+    lateinit var check1 : CheckBox
+    lateinit var check2 : CheckBox
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+        button1 = findViewById(R.id.toastButton)
+        button1.setOnClickListener { 
+            // TODO : 해야할 일
+            Toast.makeText(this, "나 클릭되었어요", Toast.LENGTH_SHORT).show()
+        }
+
+        check1 = findViewById(R.id.checkBox1)
+        check1.setText("탕수육")
+        check2 = findViewById(R.id.checkBox2)
+
+        check1.setOnClickListener {
+            val check : CheckBox = it as CheckBox
+            Toast.makeText(this, "내가 좋아하는 것은~" + check.text , Toast.LENGTH_SHORT).show()
+        }
+
+        check2.setOnClickListener {
+            val check : CheckBox = it as CheckBox
+            Toast.makeText(this, "내가 좋아하는 것은~" + check.text , Toast.LENGTH_SHORT).show()
+        }
+
     }
 }
